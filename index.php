b6efa228114965fd971867b22129fe666dec3637
Pauline_Timoff <paulinetimoff@gmail.com>
2024-05-22 16:51:09 +0300

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="=utf-8">
+    <title>php</title>
+    </head>
+    <body>
+
+        <p>Привет,мир!</p>
+
+        <?php
+
+        $name="Polina";
+        $surname="Timofeyeva";
+        
+        
+        echo "My name:\n" . $name, " " . $surname;
+
+        ?>
+
+    </body> 
+</html>
